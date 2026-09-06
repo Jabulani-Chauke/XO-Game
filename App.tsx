08f539b98889f5392426e99b1ede4934c15c979d
Jabulani-Chauke <ST10532872@myemeris.edu.za>
2026-09-06 10:13:22 +0200

    pushing iniyial code to repository

---
+++ App.tsx
@@ -0,0 +1,85 @@
+// App.tsx
+import { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component = {PlayerScreen} />
+        <Stack.Screen name="Game" component = {Gamescreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+  function PlayerScreen( {navigation} : any){
+     const [player1, setPlayer1] = useState<string>('');
+     const [player2, setPlayer2] = useState<string>('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Tic-Tac-Toe!</Text>
+
+      <TextInput
+        placeholder="Player 1 Name"
+        value={player1}
+        onChangeText={setPlayer1}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Player 2 Name"
+        value={player2}
+        onChangeText={setPlayer2}
+        style={styles.input}
+      />
+
+      <Button
+        title="Start Game"
+        onPress={() => {navigation.navigate("Game" , { 
+          player1: player1,
+          player2:player2,
+        })}}
+      />
+    </View>
+  );
+  }
+
+  function Gamescreen({navigation, route} : any) {
+    const { payer1, player2} = route.params;
+    return(
+      <view style={styles.container}>
+        <Text style={styles.title}>Game Screen</Text>
+        <Text style={styles.title}>{player1} VS {player2}</Text>
+      </view>
+    )
+  }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 12,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginVertical: 8,
+    width: '80%',
+  },
+});
